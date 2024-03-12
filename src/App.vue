94e4b2bacc41be33236caef69376f396fbe6f9d7
LoiseauCodeur <aldanmarki77@gmail.com>
2024-03-12 18:33:54 +0100

    new

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div id="map" style="width: 100%; height: 400px;"></div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+export default {
+  name: 'App',
+  mounted() {
+    let map = L.map('map').setView([48.8566, 2.3522], 2); 
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
