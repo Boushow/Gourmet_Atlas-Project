b7b8c957bff16a50599a9eb21a5220253ae336a8
Robert BADETTE <119432305+Boushow@users.noreply.github.com>
2024-03-15 11:53:03 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
-
 <template>
-  <div id="map" style="width: 100%; height: 400px;"></div>
+  <div id="map"></div>
 </template>
 
 <script>
-import L from 'leaflet';
+import L from "leaflet";
 
 export default {
-  name: 'App',
+  name: "App",
   mounted() {
-    let map = L.map('map').setView([48.8566, 2.3522], 2); 
+    let map = L.map("map").setView([43.8566, 2.3522], 2);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-  }
-}
+  },
+};
 </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
