fc6d847f0e3d9119f8a118599411c68d502c6bbd
Hudayfa Koujdal <102149280+hdif004@users.noreply.github.com>
2024-03-17 06:57:21 +0000

    fonction focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+
+const USA = L.marker([36, -95], {title : 'USA'}).addTo(map);
+
+function Focus(marqueur) {
+    let position = marqueur.getLatLng();
+    marqueur.on('click', function() {
+        map.setView(position, 3);
+    });
+}
+
+Focus(USA);
